08c993dbba5ea783f0d277d920d2f99cf0f6bb83
Yathindra S. <72652296+Yathindra-S-Rao@users.noreply.github.com>
2025-08-15 11:14:23 +0530

    added method to search-by-id aswell

--- SearchByName.py
+++ SearchByName.py
@@ -1,7 +1,6 @@
 import requests
 
 base_url = "https://gorest.co.in/public/v2/users"
-response = requests.get(base_url)
 
 
 def search_by_name(name):
@@ -9,5 +8,11 @@ def search_by_name(name):
     return requests.get(url)
 
 
-print(response.json())
-print(search_by_name("Raj").json())
+def search_by_id(id):
+    url = f"{base_url}?id={id}"
+    return requests.get(url)
+
+
+print(search_by_id("8068000").json())
+print(search_by_name("Prof. Vasudha Prajapat").json())
+
